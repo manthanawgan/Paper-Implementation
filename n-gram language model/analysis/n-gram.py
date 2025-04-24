25985d627579fbf778e3b7da1ba76ff8130625bc
Manthan Awgan <manthanawgan@gmail.com>
2025-04-24 17:02:48 +0530

    main

---
+++ n-gram language model/analysis/n-gram.py
@@ -0,0 +1,8 @@
+import numpy as np
+from nltk.util import ngrams
+from preprocess import get_tokenized_sentences
+
+class NgramCounter:
+    def __init__(self, file_name : str) -> None:
+        self.text_file
+        self.count = 
